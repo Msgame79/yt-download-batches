54f27c6618d6e2ee2784b22164a09b3de25d7a55
Msgames79 <102644909+Msgame79@users.noreply.github.com>
2024-08-23 04:02:34 +0900

    Delete batches/downloadbilibililive.bat

--- batches/downloadbilibililive.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001
-echo This can download bilibili Live. 
-set /P url=stream url 
-set output=output file name 
-yt-dlp -U
-yt-dlp -f "best[ext=flv]" -o %output%.flv %url%
-ffmpeg -i %output%.flv -c copy %output%.mp4
-del %output%.flv
