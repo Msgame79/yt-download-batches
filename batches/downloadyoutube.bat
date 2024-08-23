742a01018668fdaf24b2fdf694a6f27627b7710f
Msgames79 <102644909+Msgame79@users.noreply.github.com>
2024-08-24 04:55:26 +0900

    Delete batches directory

--- batches/downloadyoutube.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-chcp 65001
-echo This can download YouTube videos. 
-set /P url=Video URL 
-set /P output=Output File name without extension 
-yt-dlp -U
-yt-dlp -f "bestvideo[ext=mp4][protocol=https]+bestaudio[ext=m4a][protocol=https]" -o %output%.mp4 %url%
