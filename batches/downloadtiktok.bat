b5c2affd99aa3d140e95e2432b3f900282d3a1d1
Msgames79 <102644909+Msgame79@users.noreply.github.com>
2024-08-24 10:46:18 +0900

    Add files via upload

---
+++ batches/downloadtiktok.bat
@@ -0,0 +1,7 @@
+@echo off
+chcp 65001
+echo This can download TikTok videos. 
+set /P url=Video URL 
+set /P output=Output file name without extension 
+yt-dlp -U
+yt-dlp -f "best[ext=mp4][vcodec*=h264]" -o %output%.mp4 %url%
