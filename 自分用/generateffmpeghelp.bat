0e1396e106dab7994a9d49bc6189dc58f095fc52
Msgames79 <102644909+Msgame79@users.noreply.github.com>
2024-09-17 02:30:48 +0900

    Add files via upload

---
+++ 自分用/generateffmpeghelp.bat
@@ -0,0 +1,119 @@
+echo Beginning of section>help.txt
+
+ffmpeg -h full>>help.txt
+
+echo End of section>>help.txt
+
+echo Beginning of section>>help.txt
+
+ffmpeg -muxers>>help.txt
+
+echo End of section>>help.txt
+
+echo Beginning of section>>help.txt
+
+ffmpeg -demuxers>>help.txt
+
+echo End of section>>help.txt
+
+echo Beginning of section>>help.txt
+
+ffmpeg -devices>>help.txt
+
+echo End of section>>help.txt
+
+echo Beginning of section>>help.txt
+
+ffmpeg -decoders>>help.txt
+
+echo End of section>>help.txt
+
+echo Beginning of section>>help.txt
+
+ffmpeg -encoders>>help.txt
+
+echo End of section>>help.txt
+
+echo Beginning of section>>help.txt
+
+ffmpeg -filters>>help.txt
+
+echo End of section>>help.txt
+
+echo Beginning of section>>help.txt
+
+ffmpeg -pix_fmts>>help.txt
+
+echo End of section>>help.txt
+
+echo Beginning of section>>help.txt
+
+ffmpeg -layouts>>help.txt
+
+echo End of section>>help.txt
+
+echo Beginning of section>>help.txt
+
+ffmpeg -sample_fmts>>help.txt
+
+echo End of section>>help.txt
+
+echo Beginning of section>>help.txt
+
+ffmpeg -buildconf>>help.txt
+
+echo End of section>>help.txt
+
+echo Beginning of section>>help.txt
+
+ffmpeg -formats>>help.txt
+
+echo End of section>>help.txt
+
+echo Beginning of section>>help.txt
+
+ffmpeg -codecs>>help.txt
+
+echo End of section>>help.txt
+
+echo Beginning of section>>help.txt
+
+ffmpeg -bsfs>>help.txt
+
+echo End of section>>help.txt
+
+echo Beginning of section>>help.txt
+
+ffmpeg -protocols>>help.txt
+
+echo End of section>>help.txt
+
+echo Beginning of section>>help.txt
+
+ffmpeg -dispositions>>help.txt
+
+echo End of section>>help.txt
+
+echo Beginning of section>>help.txt
+
+ffmpeg -colors>>help.txt
+
+echo End of section>>help.txt
+
+echo Beginning of section>>help.txt
+
+ffmpeg -sources>>help.txt
+
+echo End of section>>help.txt
+
+echo Beginning of section>>help.txt
+
+ffmpeg -sinks>>help.txt
+
+echo End of section>>help.txt
+
+echo Beginning of section>>help.txt
+
+ffmpeg -hwaccels>>help.txt
+
+echo End of section>>help.txt
