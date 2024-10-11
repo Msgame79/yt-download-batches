6be7cc4c0345e989e47e188dafa8ab5a3134ecce
Msgames79 <102644909+Msgame79@users.noreply.github.com>
2024-10-11 12:01:13 +0900

    Update savebilibiliarchive.bat
    実行内容を可視化してみた。コマンドの順番が入れ替わってるので要検証。

--- 自分用/savebilibiliarchive.bat
+++ 自分用/savebilibiliarchive.bat
@@ -34,7 +34,7 @@ echo.
 
 echo URLを入力
 
-echo ^&は使用しないでください。なんなら^&以降は消してください
+echo ^&は使用しないでください。なんなら^&以降は消してください。
 
 echo 使用可能なURLの例
 
@@ -54,21 +54,17 @@ set /p url=
 
 echo.
 
-echo 出力ファイル名
+echo 出力ファイル名^(拡張子なし^)
 
-echo 実行後^(入力ファイル名^).zipという名前のファイルが生成されます
+echo 実行後^(入力ファイル名^).zipという名前のファイルが生成されます。
 
-echo ファイル名に使用できない文字^(\/:*?"<>|^)を入力しないでください
+echo 既にファイル名.zipが存在している場合は上書きされます。
+
+echo ファイル名に使用できない文字^(\/:*?"<>|^)を入力しないでください。
 
 echo.
 
 set /p output=
-
-del %output%.zip
-
-del /q a
-
-rmdir /q a
 
 cls
 
@@ -80,46 +76,254 @@ echo 出力ファイル名^: %output%.zip
 
 echo.
 
-winget upgrade ffmpeg
+echo 既存の%output%.zipを削除中^(1/9^)
 
-winget upgrade --id yt-dlp.yt-dlp
+del %output%.zip> nul
 
-winget upgrade --id yt-dlp.yt-dlp.nightly
+cls
 
-winget upgrade --id 7zip.7zip
+echo プレビュー
 
-winget upgrade --id 7zip.7zip.Alpha.exe
+echo URL^: %url%
 
-winget upgrade --id 7zip.7zip.Alpha.msi
+echo 出力ファイル名^: %output%.zip
 
-mkdir a
+echo.
+
+echo %output%.zip削除完了^(1/9^)
+
+echo %output%フォルダを削除中^(2/9^)
+
+del /q %output%\*> nul
+
+rmdir /q %output%> nul
+
+cls
+
+echo プレビュー
+
+echo URL^: %url%
+
+echo 出力ファイル名^: %output%.zip
+
+echo.
+
+echo %output%.zip削除完了^(1/9^)
+
+echo %output%フォルダ削除完了^(2/9^)
+
+echo 実行準備中^(3/9^)
+
+winget upgrade ffmpeg> nul
+
+winget upgrade --id yt-dlp.yt-dlp> nul
+
+winget upgrade --id yt-dlp.yt-dlp.nightly> nul
+
+winget upgrade --id 7zip.7zip> nul
+
+winget upgrade --id 7zip.7zip.Alpha.exe> nul
+
+winget upgrade --id 7zip.7zip.Alpha.msi> nul
+
+cls
+
+echo プレビュー
+
+echo URL^: %url%
+
+echo 出力ファイル名^: %output%.zip
+
+echo.
+
+echo %output%.zip削除完了^(1/9^)
+
+echo %output%フォルダ削除完了^(2/9^)
+
+echo 実行準備完了^(3/9^)
+
+echo 動画ダウンロード中^(4/9^)
+
+mkdir %output%
 
 yt-dlp -q --progress --cookies cookies.txt -f "bv+ba/best" --recode-video mp4 --write-info-json -o "%output%_%%(autonumber)04d.%%(ext)s" %url% 
 
-yt-dlp -q --progress --skip-download --write-thumbnail --convert-thumbnails png -o "a\%output%.%%(ext)s" %url% 
+cls
+
+echo プレビュー
+
+echo URL^: %url%
+
+echo 出力ファイル名^: %output%.zip
+
+echo.
+
+echo %output%.zip削除完了^(1/9^)
+
+echo %output%フォルダ削除完了^(2/9^)
+
+echo 実行準備完了^(3/9^)
+
+echo 動画ダウンロード完了^(4/9^)
+
+echo サムネイルダウンロード中^(5/9^)
+
+yt-dlp -q --progress -w --skip-download --write-thumbnail -o "a\%output%.%%(ext)s" %url% 
+
+cls
+
+echo プレビュー
+
+echo URL^: %url%
+
+echo 出力ファイル名^: %output%.zip
+
+echo.
+
+echo %output%.zip削除完了^(1/9^)
+
+echo %output%フォルダ削除完了^(2/9^)
+
+echo 実行準備完了^(3/9^)
+
+echo 動画ダウンロード完了^(4/9^)
+
+echo サムネイルダウンロード完了^(5/9^)
+
+echo 動画の処理中^(6/9^)
 
 dir /b %output%*.mp4>%output%.txt
 
-dir /b %output%*.json>%output%1.txt
-
 for /f "delims=" %%a in (%output%.txt) do (echo file %%a>>%output%2.txt)
+
+ffmpeg -loglevel -8 -f concat -i %output%2.txt -c copy a\%output%.mp4
+
+cls
+
+echo プレビュー
+
+echo URL^: %url%
+
+echo 出力ファイル名^: %output%.zip
+
+echo.
+
+echo %output%.zip削除完了^(1/9^)
+
+echo %output%フォルダ削除完了^(2/9^)
+
+echo 実行準備完了^(3/9^)
+
+echo 動画ダウンロード完了^(4/9^)
+
+echo サムネイルダウンロード完了^(5/9^)
+
+echo 動画処理完了^(6/9^)
+
+echo jsonファイル処理中^(7/9^)
+
+dir /b %output%*.json>%output%1.txt
 
 for /f "delims=" %%b in (%output%1.txt) do (type %%b>>%output%.json& echo.>>%output%.json)
 
-ffmpeg -loglevel -8 -f concat -i %output%2.txt -c copy a\%output%.mp4 
+powershell -Command "Get-Content %output%.json -Encoding UTF8 | ForEach-Object { [System.Net.WebUtility]::HtmlDecode($_) } | Out-File a\%output%.json"> nul
 
-powershell -Command "Get-Content %output%.json -Encoding UTF8 | ForEach-Object { [System.Net.WebUtility]::HtmlDecode($_) } | Out-File a\%output%.json" 
+cls
 
-del /q %output%*
+echo プレビュー
 
-cd a
+echo URL^: %url%
 
-7z a %output%.zip *
+echo 出力ファイル名^: %output%.zip
 
-move %output%.zip ..
+echo.
+
+echo %output%.zip削除完了^(1/9^)
+
+echo %output%フォルダ削除完了^(2/9^)
+
+echo 実行準備完了^(3/9^)
+
+echo 動画ダウンロード完了^(4/9^)
+
+echo サムネイルダウンロード完了^(5/9^)
+
+echo 動画処理完了^(6/9^)
+
+echo jsonファイル処理完了^(7/9^)
+
+echo zip作成中^(8/9^)
+
+cd %output%
+
+7z a %output%.zip *> nul
+
+move %output%.zip ..> nul
 
 cd ..
 
-del /q a
+cls
 
-rmdir a
+echo プレビュー
+
+echo URL^: %url%
+
+echo 出力ファイル名^: %output%.zip
+
+echo.
+
+echo %output%.zip削除完了^(1/9^)
+
+echo %output%フォルダ削除完了^(2/9^)
+
+echo 実行準備完了^(3/9^)
+
+echo 動画ダウンロード完了^(4/9^)
+
+echo サムネイルダウンロード完了^(5/9^)
+
+echo 動画処理完了^(6/9^)
+
+echo jsonファイル処理完了^(7/9^)
+
+echo zip作成完了^(8/9^)
+
+echo 不要なファイルを削除中^(9/9^)
+
+del /q %output%*> nul
+
+del /q %output%\*> nul
+
+rmdir %output%> nul
+
+cls
+
+echo プレビュー
+
+echo URL^: %url%
+
+echo 出力ファイル名^: %output%.zip
+
+echo.
+
+echo %output%.zip削除完了^(1/9^)
+
+echo %output%フォルダ削除完了^(2/9^)
+
+echo 実行準備完了^(3/9^)
+
+echo 動画ダウンロード完了^(4/9^)
+
+echo サムネイルダウンロード完了^(5/9^)
+
+echo 動画処理完了^(6/9^)
+
+echo jsonファイル処理完了^(7/9^)
+
+echo zip作成完了^(8/9^)
+
+echo 不要なファイルの削除完了^(9/9^)
+
+echo 実行完了。10秒後に自動で終了します。
+
+timeout /t 10 /nobreak> nul
