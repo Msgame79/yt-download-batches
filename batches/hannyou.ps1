473087646b7fbf799ad9d1a22399c7d5d7a52598
Msgames79 <102644909+Msgame79@users.noreply.github.com>
2025-02-12 22:18:07 +0900

    Update hannyou.ps1

--- batches/hannyou.ps1
+++ batches/hannyou.ps1
@@ -10,8 +10,9 @@ Set-Location $PSScriptRoot
 [string]$cookies = ""
 [array]$list = @()
 [string]$codec = ""
+[string]$thumbnailconfirm = "Y"
 
-# check if ffmpeg and ffprobe are available
+# check if ffmpeg, ffprobe and yt-dlp are available
 ffprobe -version | Out-Null
 if (-not $?) {
     "ffprobe is unabailable`nenter to exit"
@@ -24,50 +25,73 @@ if (-not $?) {
     Read-Host
     exit
 }
+yt-dlp --version | Out-Null
+if (-not $?) {
+    "yt-dlp is unabailable`nenter to exit"
+    Read-Host
+    exit
+}
 
-# main block
+# main area
 while ($true) {
-    do {
+    do { # yt-dlpがダウンロードできるURLを書くまで繰り返し
         Clear-Host
         $url = Read-Host -Prompt "URL"
-        Invoke-WebRequest -Uri "$url" -Method Head | Out-Null
+        yt-dlp -F $url | Out-Null
     } until ($?)
-    do {
+    do { # 出力ファイル名を指定(上書きとファイル名に使ってほしくない文字を回避)
+        Clear-Host
+        "URL: $url"
         $output = Read-Host -Prompt "output filename without extension"
         $len = $output.Length
-    } until ($len -ge 1 -and $output -match "[^\u0022\u002a\u002f\u003a\u003c\u003e\u003f\u005c\u007c]{$len}")
-    if (Test-Path -Path "$output.mp4") {
-        "file already exists`nenter to return"
-        continue
-    }
+    } until ($len -ge 1 -and $len -le 255 -and $output -match "[^\u0020\u0022\u002a\u002f\u003a\u003c\u003e\u003f\u005c\u007c\u3000]{$len}" -and -not (Test-Path "$output.mp4"))
+    do {
+        Clear-Host
+        "URL: $url"
+        "output filename without extension: $output"
+        $thumbnailconfirm = Read-Host -Prompt "Download thumbnail?(yYnN)"
+    } until ($thumbnailconfirm -match "^[yYnN]$")
     if (Test-Path -Path "cookies.txt") {
+        "I use cookies.txt"
         $cookies = "--cookies cookies.txt"
     } else {
+        "I don't use cookies.txt"
         $cookies = "--no-cookies"
     }
-    yt-dlp -q --progress --force-overwrites -f "bv[ext=mp4]+ba[ext=m4a]/bv+ba/best" --recode-video mp4 $cookies -o "${output}_%(autonumber)d.mp4" -R infinite $url
-    yt-dlp -q --force-overwrites --skip-download --write-thumbnail --convert-thumbnails png -o "$output.%(ext)s" $url
+    "downloading video"
+    yt-dlp -q --progress --force-overwrites -f "bv[ext=mp4]+ba[ext=m4a]/bv+ba/best" $cookies -o "${output}_%(autonumber)d.%(ext)s" -R infinite $url
     if (Test-Path "list.txt") {
-        Remove-Item "list.txt"
+        "deleting list.txt"
+        do {
+            Remove-Item "list.txt"
+        } until ($?)
     }
-    $list = Get-ChildItem -Name | Where-Object {$_ -match "${output}_\d+\.mp4"}
+    $list = Get-ChildItem -Name | Where-Object {$_ -match "${output}_\d+\.[^\.]+"}
     if ($list.Count -ge 2) {
+    } else {
+        "connecting videos"
         $list | ForEach-Object {"file $_" | Out-File "list.txt" -Append -Encoding utf8NoBOM}
         ffmpeg -y -loglevel -8 -f concat -i "list.txt" -c:v h264_nvenc -qmax 18 -qmin 18 "${output}1.mp4"
         $list | ForEach-Object {Remove-Item $_}
-    } else {
-        Rename-Item "${output}_1.mp4" "${output}1.mp4"
-    }
-    if (Test-Path "list.txt") {
-        Remove-Item "list.txt"
+        "deleting list.txt"
+        do {
+            Remove-Item "list.txt"
+        } until ($?)
     }
     $list = @()
     $codec = ffprobe -hide_banner -loglevel 16 -of "default=nw=1:nk=1" -select_streams v:0 -show_entries "stream=codec_name" "${output}1.mp4"
     if (-not $codec -match "^h264$") {
+        "transcoding to h264"
         ffmpeg -y -loglevel -8 -i "${output}1.mp4" -c:v h264_nvenc -qmax 18 -qmin 18 -c:a copy "${output}.mp4"
     } else {
         Rename-Item "${output}1.mp4" "${output}.mp4"
     }
-    "${output}.mp4 saved successfully`n enter to continue"
+    if ($thumbnailconfirm -match "[yY]") {
+        "downloading thumbnail(${output}.png)"
+        yt-dlp -q --force-overwrites --skip-download --write-thumbnail --convert-thumbnails png -o "$output.%(ext)s" $url
+        "${output}.mp4 and ${output}.png saved successfully`nenter to continue"
+    } else {
+        "${output}.mp4 saved successfully`nenter to continue"
+    }
     Read-Host
 }
