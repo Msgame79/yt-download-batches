fb98d3dacc9fad1ac7d1bdeed5e56e7a2128c558
Msgames79 <102644909+Msgame79@users.noreply.github.com>
2025-02-22 16:08:11 +0900

    サムネを2MB以下にする

--- batches/hannyou_automation.ps1
+++ batches/hannyou_automation.ps1
@@ -7,31 +7,41 @@ Set-Location $PSScriptRoot
 [string]$output=""
 [string]$url=""
 
-if ($args[0] -ne "") {
-    $output = $args[0]+"_%(autonumber)d.%(ext)s"
+if ($null -ne $args[0]) {
+    $output = $args[0]
+} else {
+    exit
 }
-if ($args[1] -ne "") {
+if ($null -ne $args[1]) {
     $url = $args[1]
+} else {
+    exit
 }
 
-yt-dlp -q --progress --force-overwrites -f "bv[ext=mp4]+ba[ext=m4a]/bv+ba/best" -o "${output}" -R infinite "${url}"
-$list = Get-ChildItem -Name | Where-Object {$_ -match "${output}_\d+\.[^\.]+"}
-if ($list.Count -ge 2) {
-} else {
-    $list | ForEach-Object {"file ""$_""" | Out-File "list.txt" -Append -Encoding utf8NoBOM}
-    ffmpeg -y -loglevel -8 -f concat -i "list.txt" -c:v h264_nvenc -qmax 18 -qmin 18 "${output}1.mp4"
-    $list | ForEach-Object {Remove-Item $_}
-    "deleting list.txt"
-    do {
-        Remove-Item "list.txt"
-    } until ($?)
+yt-dlp -q --progress --force-overwrites -f "bv[ext=mp4]+ba[ext=m4a]/bv+ba/best" -o "${output}_%(autonumber)d.%(ext)s" -R infinite "${url}"
+if ($?) {
+    $list = Get-ChildItem -Name | Where-Object {$_ -match "${output}_\d+\.[^\.]+"}
+    if ($list.Count -eq 1) {
+        Rename-Item "${output}_1.mp4" "${output}1.mp4"
+    } else {
+        $list | ForEach-Object {"file ""$_""" | Out-File "list.txt" -Append -Encoding utf8NoBOM}
+        ffmpeg -y -loglevel -8 -f concat -i "list.txt" -c:v h264_nvenc -qmax 18 -qmin 18 "${output}1.mp4"
+        $list | ForEach-Object {Remove-Item $_}
+        do {
+            Remove-Item "list.txt"
+        } until ($?)
+    }
+    $codec = ffprobe -hide_banner -loglevel 16 -of "default=nw=1:nk=1" -select_streams v:0 -show_entries "stream=codec_name" "${output}1.mp4"
+    if (-not $codec -match "^h264$") {
+        ffmpeg -y -loglevel -8 -i "${output}1.mp4" -c:v h264_nvenc -qmax 18 -qmin 18 -c:a copy "${output}.mp4"
+    } else {
+        Rename-Item "${output}1.mp4" "${output}.mp4"
+    }
+    yt-dlp -q --force-overwrites --skip-download --write-thumbnail --convert-thumbnails png -o "${output}" -R infinite "${url}"
+    if ((Get-ItemProperty "${output}.png").Length -ge 2097152) {
+        ffmpeg -hide_banner -loglevel 0 -y -i "${output}.png" -c mjpeg -q 0 -frames:v 1 "${output}.jpg"
+        do {
+            Remove-Item "${output}.png"
+        } until ($?)
+    }
 }
-$list = @()
-$codec = ffprobe -hide_banner -loglevel 16 -of "default=nw=1:nk=1" -select_streams v:0 -show_entries "stream=codec_name" "${output}1.mp4"
-if (-not $codec -match "^h264$") {
-    "transcoding to h264"
-    ffmpeg -y -loglevel -8 -i "${output}1.mp4" -c:v h264_nvenc -qmax 18 -qmin 18 -c:a copy "${output}.mp4"
-} else {
-    Rename-Item "${output}1.mp4" "${output}.mp4"
-}
-yt-dlp -q --force-overwrites --skip-download --write-thumbnail --convert-thumbnails png -o "${output}.png" -R infinite "${url}"
