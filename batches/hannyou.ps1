23e1e6702c2d6e8d0df8c543437bf491238263fb
Msgames79 <102644909+Msgame79@users.noreply.github.com>
2025-02-22 15:27:10 +0900

    cookieの確認が意味ないところにあった

--- batches/hannyou.ps1
+++ batches/hannyou.ps1
@@ -1,8 +1,9 @@
 # ps1 encoding
 chcp 65001
 
+Set-Location $PSScriptRoot
+
 # default values
-[string]$wd = "$PSScriptRoot"
 [string]$url = ""
 [string]$output = ""
 [int]$len = 0
@@ -10,11 +11,6 @@ chcp 65001
 [array]$list = @()
 [string]$codec = ""
 [string]$thumbnailconfirm = "Y"
-[string]$vencodesetting = "-c:v h264_nvenc -qmax 18 -qmin 18"
-[string]$aencodesetting = "-c:a copy"
-
-$ErrorActionPreference = 'SilentlyContinue'
-Set-Location $wd
 
 # check if ffmpeg, ffprobe and yt-dlp are available
 ffprobe -version | Out-Null
@@ -40,29 +36,26 @@ if (-not $?) {
 while ($true) {
     do { # yt-dlpがダウンロードできるURLを書くまで繰り返し
         Clear-Host
-        "Current Setting`nDirectory: $wd`nVideo encoding setting: $vencodesetting`nAudio encoding setting: $aencodesetting"
-        $url = Read-Host -Prompt "URL"
-        if (Test-Path -Path ".\cookies.txt") {
-            $cookies = "--cookies .\cookies.txt"
-        } else {
-            $cookies = "--no-cookies"
-        }
-        yt-dlp -F $cookies $url | Out-Null
+        do {
+            $url = Read-Host -Prompt "URL"
+        } until ($url -match "^https://[^ ]+")
+        yt-dlp -F $url | Out-Null
     } until ($?)
     do { # 出力ファイル名を指定(上書きとファイル名に使ってほしくない文字を回避)
         Clear-Host
-        "Current Setting`nDirectory: $wd`nVideo encoding setting: $vencodesetting`nAudio encoding setting: $aencodesetting`nURL: $url"
+        "URL: $url"
         $output = Read-Host -Prompt "output filename without extension"
         $len = $output.Length
     } until ($len -ge 1 -and $len -le 255 -and $output -match "[^\u0020\u0022\u002a\u002f\u003a\u003c\u003e\u003f\u005c\u007c\u3000]{$len}" -and -not (Test-Path "$output.mp4"))
     do {
         Clear-Host
-        "Current Setting`nDirectory: $wd`nVideo encoding setting: $vencodesetting`nAudio encoding setting: $aencodesetting`nURL: $url`noutput filename without extension: $output"
+        "URL: $url"
+        "output filename without extension: $output"
         $thumbnailconfirm = Read-Host -Prompt "Download thumbnail?(yYnN)"
     } until ($thumbnailconfirm -match "^[yYnN]$")
-    if (Test-Path -Path ".\cookies.txt") {
+    if (Test-Path -Path "cookies.txt") {
         "I use cookies.txt"
-        $cookies = "--cookies .\cookies.txt"
+        $cookies = "--cookies cookies.txt"
     } else {
         "I don't use cookies.txt"
         $cookies = "--no-cookies"
@@ -77,33 +70,30 @@ while ($true) {
     }
     $list = Get-ChildItem -Name | Where-Object {$_ -match "${output}_\d+\.[^\.]+"}
     if ($list.Count -ge 2) {
+    } else {
         "connecting videos"
         $list | ForEach-Object {"file $_" | Out-File "list.txt" -Append -Encoding utf8NoBOM}
-        ffmpeg -y -loglevel -8 -f concat -i "list.txt" $vencodesetting $aencodesetting "${output}1.mp4"
+        ffmpeg -y -loglevel -8 -f concat -i "list.txt" -c:v h264_nvenc -qmax 18 -qmin 18 "${output}1.mp4"
         $list | ForEach-Object {Remove-Item $_}
-        if (Test-Path "list.txt") {
-            "deleting list.txt"
-            do {
-                Remove-Item "list.txt"
-            } until ($?)
-        }
-    } else {
-        Rename-Item "${output}_1.mp4" "${output}1.mp4"
+        "deleting list.txt"
+        do {
+            Remove-Item "list.txt"
+        } until ($?)
     }
     $list = @()
     $codec = ffprobe -hide_banner -loglevel 16 -of "default=nw=1:nk=1" -select_streams v:0 -show_entries "stream=codec_name" "${output}1.mp4"
     if (-not $codec -match "^h264$") {
         "transcoding to h264"
-        ffmpeg -y -loglevel -8 -i "${output}1.mp4" $vencodesetting $aencodesetting "${output}.mp4"
+        ffmpeg -y -loglevel -8 -i "${output}1.mp4" -c:v h264_nvenc -qmax 18 -qmin 18 -c:a copy "${output}.mp4"
     } else {
         Rename-Item "${output}1.mp4" "${output}.mp4"
     }
     if ($thumbnailconfirm -match "[yY]") {
         "downloading thumbnail(${output}.png)"
         yt-dlp -q --force-overwrites --skip-download --write-thumbnail --convert-thumbnails png -o "$output.%(ext)s" $url
-        "${output}.mp4 and ${output}.png saved successfully`nenter to restart"
+        "${output}.mp4 and ${output}.png saved successfully`nenter to continue"
     } else {
-        "${output}.mp4 saved successfully`nenter to restart"
+        "${output}.mp4 saved successfully`nenter to continue"
     }
     Read-Host
 }
