c517128ff37279b1b84342e74c393af1fe357ff6
Msgames79 <102644909+Msgame79@users.noreply.github.com>
2025-03-22 10:07:12 +0900

    koredeoke

--- batches/hannyou.ps1
+++ batches/hannyou.ps1
@@ -1,99 +1,171 @@
-# ps1 encoding
-chcp 65001
+$ErrorActionPreference = 'SilentlyContinue'
 
-Set-Location $PSScriptRoot
+# 編集可能な変数
+[string]$defaultfolder = "$PSScriptRoot" # デフォルト: $PSScriptRoot
+[string]$formatselector = "bv+ba/best" # デフォルト: "bv+ba/best"
+[string]$thumbnailextension = "png" # デフォルト: "png" 使用可能な拡張子: png,jpg,ewbp
+[string]$vencodesetting = "-c:v libx264 -crf 21" # デフォルト: "-c:v libx264 -crf 21" フィルターをかけるので"-c:v copy"は使えない
+[string]$aencodesetting = "-c:a aac -q:a 1" # デフォルト: "-c:a aac -q:a 1" デフォルトではあえて再エンコードするように書いているが、できるなら"-c:a copy"が良い
+[string]$outputextension = "mp4" # デフォルト: "mp4" デフォルトがmp4向けのエンコード設定のため。ただし上のエンコード設定によっては変える必要あり、あとここで編集させているのはすぐ上にエンコード設定があるから。使用可能な拡張子: avi, flv, gif, mkv, mov, mp4, webm, aac, aiff, alac, flac, m4a, mka, mp3, ogg, opus, vorbis, wav
+<#
+目的別いろんなエンコードメモ
+再生できればいいからとにかく容量を小さくしたい場合
+hevc+Opus,mkv Container(環境によるかも)
+[string]$vencodesetting = "-c:v libx265 -qp 18"
+[string]$aencodesetting = "-c:a libopus -b:a 96k"
+[string]$outputextension = "mkv"
 
-# default values
+互換性が欲しい場合
+h264+aac, mp4 container(伝統的な組み合わせで大抵のデバイスで再生可能)
+[string]$vencodesetting = "-c:v libx264 -qp 18"
+[string]$aencodesetting = "-c:a aac -q:a 1"
+[string]$outputextension = "mp4"
+VP9+Opus, webm container(YouTubeでも使われてる)
+[string]$vencodesetting = "-c:v libvpx-vp9"
+[string]$aencodesetting = "-c:a libopus -b:a 96k"
+[string]$outputextension = "webm"
+
+可逆圧縮したい場合
+h264(lossless)+alac, mp4 container(mp4で可逆圧縮したい場合。flacはmp4コンテナに入らない)
+[string]$vencodesetting = "-c:v libx264 -qp 0"
+[string]$aencodesetting = "-c:a flac"
+[string]$outputextension = "mp4"
+utvideo+flac, mkv container (ファイルサイズがでかくなる)
+[string]$vencodesetting = "-c:v utvideo"
+[string]$aencodesetting = "-c:a flac"
+[string]$outputextension = "mkv"
+ffv1+flac, mkv container(ファイルの保存に一番向いている)
+[string]$vencodesetting = "-c:v ffv1 -level 3"
+[string]$aencodesetting = "-c:a flac"
+[string]$outputextension = "mkv"
+VP9(lissless)+Opus(非可逆圧縮だがWebm側が可逆圧縮の音声コーデックをサポートしていない), webm container(エンコードがめちゃ遅い)
+[string]$vencodesetting = "-c:v libvpx-vp9 -lossless 1"
+[string]$aencodesetting = "-c:a libopus -b:a 128k"
+[string]$outputextension = "webm"
+#>
+
+# 編集不可能な変数
+[string]$logtext = ""
 [string]$url = ""
-[string]$output = ""
-[int]$len = 0
+[string]$startat = ""
+[string]$endat = ""
+[UInt64]$starttime = 0
+[UInt64]$endtime = 0
+[string]$thumbnailselector = ""
 [string]$cookies = ""
-[array]$list = @()
-[string]$codec = ""
-[string]$thumbnailconfirm = "Y"
+[string]$outputfilename = ""
+[string]$files = ""
+[string]$guid = (New-Guid).Guid
+[System.Object]$processlength = $null
 
-# check if ffmpeg, ffprobe and yt-dlp are available
-ffprobe -version | Out-Null
+# メイン処理
+Set-Location $defaultfolder
+yt-dlp --version | Out-Null
 if (-not $?) {
-    "ffprobe is unabailable`nenter to exit"
+    "yt-dlpが使用できません`nEnterで終了"
     Read-Host
     exit
 }
 ffmpeg -version | Out-Null
 if (-not $?) {
-    "ffmpeg is unabailable`nenter to exit"
+    "ffmpegが使用できません`nEnterで終了"
     Read-Host
     exit
 }
-yt-dlp --version | Out-Null
-if (-not $?) {
-    "yt-dlp is unabailable`nenter to exit"
+if ($thumbnailextension -notmatch "^(jpg|png|webp)$") {
+    "`$thumbnailextensionの値が不正です`nEnterで終了"
     Read-Host
     exit
 }
-
-# main area
-while ($true) {
-    do { # yt-dlpがダウンロードできるURLを書くまで繰り返し
-        Clear-Host
-        do {
-            $url = Read-Host -Prompt "URL"
-        } until ($url -match "^https?://[^ ]+")
-        yt-dlp -F $url | Out-Null
-    } until ($?)
-    do { # 出力ファイル名を指定(上書きとファイル名に使ってほしくない文字を回避)
-        Clear-Host
-        "URL: $url"
-        $output = Read-Host -Prompt "output filename without extension"
-        $len = $output.Length
-    } until ($len -ge 1 -and $len -le 255 -and $output -match "[^\u0020\u0022\u002a\u002f\u003a\u003c\u003e\u003f\u005c\u007c\u3000]{$len}" -and -not (Test-Path "$output.mp4"))
+if ($outputextension -notmatch "^(avi|flv|gif|mkv|mov|mp4|webm|aac|aiff|alac|flac|m4a|mka|mp3|ogg|opus|vorbis|wav)$") {
+    "`$outputextensionの値が不正です`nEnterで終了"
+    Read-Host
+    exit
+}
+$logtext += "動画エンコード設定`n${vencodesetting}`n音声エンコード設定`n${aencodesetting}`n出力ファイル拡張子`n${outputextension}`nサムネイル拡張子`n${thumbnailextension}`nURL"
+do {
+    Clear-Host
+    $url = Read-Host -Prompt $logtext
+    yt-dlp -q -F "${url}" | Out-Null
+} until ($?)
+if ((yt-dlp -F "${url}"| Where-Object {$_ -match "^\[info\]"}).Count -eq 1) {
+    $logtext += ": ${url}`n開始時間(秒(.ミリ秒)または((時間:)分:)秒(.ミリ秒)表記、0で最初を指定、-1で次をスキップし、最初から最後までダウンロード)"
     do {
         Clear-Host
-        "URL: $url"
-        "output filename without extension: $output"
-        $thumbnailconfirm = Read-Host -Prompt "Download thumbnail?(yYnN)"
-    } until ($thumbnailconfirm -match "^[yYnN]$")
-    if (Test-Path -Path "cookies.txt") {
-        "I use cookies.txt"
-        $cookies = "--cookies cookies.txt"
+        $startat = Read-Host -Prompt $logtext
+    } until ($startat -match "^(0|-1|[^\d]*\d+([^\d]+\d{1,3}[^\d]*)?|[^\d]*(\d+[^\d]+)?([0-5]?\d[^\d]+)?[0-5]?\d([^\d]+\d{1,3}[^\d]*)?)$")
+    $logtext += ": ${startat}`n"
+    if ($startat -match "^-1$") {
+        $startat = "0"
+        $endat = "inf"
     } else {
-        "I don't use cookies.txt"
-        $cookies = "--no-cookies"
-    }
-    "downloading video"
-    yt-dlp -q --progress --force-overwrites -f "bv[ext=mp4]+ba[ext=m4a]/bv+ba/best" $cookies -o "${output}_%(autonumber)d.%(ext)s" -R infinite $url
-    if (Test-Path "list.txt") {
-        "deleting list.txt"
+        if ($startat -match "^([^\d]*(?<second>\d+)([^\d]+(?<millisecond>\d{1,3})[^\d]*)?|[^\d]*((?<hour>\d+)[^\d]+)?((?<minute>[0-5]?\d)[^\d]+)?(?<second>[0-5]?\d)([^\d]+(?<millisecond>\d{1,3})[^\d]*)?)$") {
+            $starttime = [uint]$Matches.hour * 3600000 + [uint]$Matches.minute * 60000 + [uint]$Matches.second * 1000 + [uint]($Matches.millisecond).PadRight(3,("0"))
+        }
+        $logtext += "終了時間(秒(.ミリ秒)または((時間:)分:)秒(.ミリ秒)表記、-1またはinfで最後までダウンロード)"
         do {
-            Remove-Item "list.txt"
-        } until ($?)
+            do {
+                Clear-Host
+                $endat = Read-Host -Prompt $logtext
+            } until ($endat -match "^(-1|inf|[^\d]*\d+([^\d]+\d{1,3}[^\d]*)?|[^\d]*(\d+[^\d]+)?([0-5]?\d[^\d]+)?[0-5]?\d([^\d]+\d{1,3}[^\d]*)?)$")
+            if ($endat -match "^([^\d]*(?<second>\d+)([^\d]+(?<millisecond>\d{1,3})[^\d]*)?|[^\d]*((?<hour>\d+)[^\d]+)?((?<minute>[0-5]?\d)[^\d]+)?(?<second>[0-5]?\d)([^\d]+(?<millisecond>\d{1,3})[^\d]*)?)$") {
+                $endtime = [uint]$Matches.hour * 3600000 + [uint]$Matches.minute * 60000 + [uint]$Matches.second * 1000 + [uint]($Matches.millisecond).PadRight(3,("0"))
+            } else {
+                $endat = "inf"
+            }
+        } until ($endat -cmatch "^inf$" -or $endtime -gt $starttime)
+        $logtext += ": ${endat}`n"
     }
-    $list = Get-ChildItem -Name | Where-Object {$_ -match "${output}_\d+\.[^\.]+"}
-    if ($list.Count -ge 2) {
-    } else {
-        "connecting videos"
-        $list | ForEach-Object {"file $_" | Out-File "list.txt" -Append -Encoding utf8NoBOM}
-        ffmpeg -y -loglevel -8 -f concat -i "list.txt" -c:v h264_nvenc -qmax 18 -qmin 18 "${output}1.mp4"
-        $list | ForEach-Object {Remove-Item $_}
-        "deleting list.txt"
-        do {
-            Remove-Item "list.txt"
-        } until ($?)
-    }
-    $list = @()
-    $codec = ffprobe -hide_banner -loglevel 16 -of "default=nw=1:nk=1" -select_streams v:0 -show_entries "stream=codec_name" "${output}1.mp4"
-    if (-not $codec -match "^h264$") {
-        "transcoding to h264"
-        ffmpeg -y -loglevel -8 -i "${output}1.mp4" -c:v h264_nvenc -qmax 18 -qmin 18 -c:a copy "${output}.mp4"
-    } else {
-        Rename-Item "${output}1.mp4" "${output}.mp4"
-    }
-    if ($thumbnailconfirm -match "[yY]") {
-        "downloading thumbnail(${output}.png)"
-        yt-dlp -q --force-overwrites --skip-download --write-thumbnail --convert-thumbnails png -o "$output.%(ext)s" $url
-        "${output}.mp4 and ${output}.png saved successfully`nenter to continue"
-    } else {
-        "${output}.mp4 saved successfully`nenter to continue"
-    }
-    Read-Host
+} else {
+    $startat = "0"
+    $endat = "inf"
 }
+$logtext += "サムネイルの処理、0=ダウンロードしない、1=動画とは別にダウンロード、2=動画へ埋め込む(画像は保存しない)、3=埋め込みと画像で保存"
+do {
+    Clear-Host
+    $thumbnailselector = Read-Host -Prompt $logtext
+} until ($thumbnailselector)
+$logtext += ": ${thumbnailselector}`n出力ファイル名(拡張子なし)"
+do {
+    Clear-Host
+    $outputfilename = Read-Host -Prompt $logtext
+} until ($outputfilename -notmatch "[\u0020\u0022\u002a\u002f\u003a\u003c\u003e\u003f\u005c\u007c\u3000]" -and (Get-ChildItem -Name -File | Where-Object {$_ -cmatch "^${outputfilename}(1|_\d{3})?\.(${outputextension}|${thumbnailextension})$"}).Count -eq 0)
+Write-Host -Object "${logtext}: ${outputfilename}"
+if (Test-Path -Path ".\cookies.txt") {
+    Write-Host -Object "cookies.txtを使用します"
+    $cookies = "--cookies cookies.txt"
+} else {
+    Write-Host -Object "cookies.txtが見つかりません"
+    $cookies = "--no-cookies"
+}
+$processlength = Measure-Command -Expression {
+    Write-Host -Object "(yt-dlp)動画ダウンロード中"
+    yt-dlp --quiet --progress --download-sections "*${startat}-${endat}" $cookies --format """${formatselector}""" --downloader-args "ffmpeg_i:-loglevel quiet" --downloader-args "ffmpeg_o:${vencodesetting} ${aencodesetting}" --remux_video $outputextension --output "${outputfilename}_%(autonumber)03d.%(ext)s" --retries infinite $url
+    if ((Get-ChildItem -Name | Where-Object {$_ -match "${outputfilename}_\d{3}\.$outputextension$"}).Count - 1) {
+        Get-ChildItem -Name | Where-Object {$_ -match "${outputfilename}_\d{3}\.$outputextension$"} | ForEach-Object {$files += "file ${_}`n"}
+        New-Item -Path ".\${guid}.txt" -ItemType File -Value $files
+        Write-Host -Object "(ffmpeg)動画を結合中"
+        ffmpeg -loglevel -8 -f concat -i "${guid}.txt" -c copy "${outputfilename}1.${outputextension}"
+        Get-ChildItem -Name | Where-Object {$_ -match "${outputfilename}_\d{3}\.$outputextension$"} | ForEach-Object {Remove-Item $_}
+        Remove-Item ".\${guid}.txt"
+    } else {
+        Rename-Item "$(Get-ChildItem -Name | Where-Object {$_ -match "${outputfilename}_001\.$outputextension$"})" "${outputfilename}1.${outputextension}"
+    }
+    if ([int]$thumbnailselector) {
+        Write-Host -Object "(yt-dlp)サムネイルダウンロード中"
+        yt-dlp --quiet --max-downloads 1 --skip-download --write-thumbnail --convert-thumbnails $thumbnailextension $cookies --output "${outputfilename}.%(ext)s" $url
+        if ([int]$thumbnailselector - 1) {
+            ffmpeg -i "${outputfilename}1.${outputextension}" -i "${outputfilename}.${thumbnailextension}" -map 0 -map 1 -c copy -disposition:v:1 attached_pic "${outputfilename}.${outputextension}"
+            Remove-Item "${outputfilename}1.${outputextension}"
+            if ([int]$thumbnailselector -eq 2) {
+                Remove-Item "${outputfilename}.${thumbnailextension}"
+            }
+        } else {
+            Rename-Item "${outputfilename}1.${outputextension}" "${outputfilename}.${outputextension}"
+        }
+    } else {
+        Rename-Item "${outputfilename}1.${outputextension}" "${outputfilename}.${outputextension}"
+    }
+}
+"ダウンロードにかかった時間: $((($processlength.Hours).ToString()).PadLeft(2,'0')):$((($processlength.Minutes).ToString()).PadLeft(2,'0')):$((($processlength.Seconds).ToString()).PadLeft(2,'0')).$((($processlength.Milliseconds).ToString()).PadLeft(3,'0'))`nEnterで終了"
+Write-Host
