59b9c2e23f2a7feca991e3bb2b73a8a28c706852
Msgames79 <102644909+Msgame79@users.noreply.github.com>
2025-10-13 20:55:59 +0900

    oke

--- 自分用/for人类一败途地動画保管庫/download.ps1
+++ 自分用/for人类一败途地動画保管庫/download.ps1
@@ -1,6 +1,5 @@
 chcp 65001
 $ErrorActionPreference = 'SilentlyContinue'
-Clear-Host
 # Needs 2 arguments
 if (($args).Count -eq 2)
 {
@@ -48,6 +47,7 @@ if (($args).Count -eq 2)
             Get-ChildItem -Name "$($num)" | Where-Object {$_ -ne "output$($num).mp4"} | ForEach-Object {Remove-Item "$($num)\$($_)"}
             Start-Process "yt-dlp" "--quiet --max-downloads 1 --skip-download --write-thumbnail --convert-thumbnails png -R infinite -o ""$($num)\$($args[0]).%(ext)s"" https://www.bilibili.com/video/$($bv)" -Wait -NoNewWindow
             Start-Process "yt-dlp" "--quiet --max-downloads 1 --skip-download --write-thumbnail --convert-thumbnails jpg -R infinite -o ""$($num)\$($args[0]).%(ext)s"" https://www.bilibili.com/video/$($bv)" -Wait -NoNewWindow
+            "Download completed`n($(ffprobe -hide_banner -i "$num\$($args[0]).mp4" -loglevel 0 -select_streams v -of "default=nw=1:nk=1" -show_entries "stream=coded_width")x$(ffprobe -hide_banner -i "$num\$($args[0]).mp4" -loglevel 0 -select_streams v -of "default=nw=1:nk=1" -show_entries "stream=coded_height"))"
             exit(0)
         }
         else
