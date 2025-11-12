6c7f9c4222edcf9efa72be4cead1efff076c30b0
Msgames79 <102644909+Msgame79@users.noreply.github.com>
2025-11-12 23:43:04 +0900

    aiueo

--- 自分用/for人类一败途地動画保管庫/download.ps1
+++ 自分用/for人类一败途地動画保管庫/download.ps1
@@ -51,7 +51,7 @@ if (($args).Count -eq 2)
                 $ins1 += "[${a}:v]scale=s=hd1080[v$($a+1)];"
                 $a++
                 $ins2 += "[v$a][$($a-1):a]"
-                $scales += "($(ffprobe -hide_banner -i "$num\$_.mp4" -loglevel 0 -select_streams v -of "default=nw=1:nk=1" -show_entries "stream=coded_width")x$(ffprobe -hide_banner -i "$num\$_.mp4" -loglevel 0 -select_streams v -of "default=nw=1:nk=1" -show_entries "stream=coded_width"))"
+                $scales += "($(ffprobe -hide_banner -i "$num\$_" -loglevel 0 -select_streams v -of "default=nw=1:nk=1" -show_entries "stream=coded_width")x$(ffprobe -hide_banner -i "$num\$_" -loglevel 0 -select_streams v -of "default=nw=1:nk=1" -show_entries "stream=coded_width"))"
             }
             Start-Process "ffmpeg" "-loglevel -8 ${ins}-filter_complex ""${ins1}${ins2}concat=n=${a}:v=1:a=1[v][a]"" -map ""[v]"" -map ""[a]"" -c:v h264_nvenc -qp 21 -c:a aac -b:a 192k $($num)\$($args[0]).mp4" -Wait -NoNewWindow
             Get-ChildItem -Name "$($num)" | Where-Object {$_ -ne "output$($num).mp4"} | ForEach-Object {Remove-Item "$($num)\$($_)"}
